5f744fee076c3d400af804ab2cee3e28da9b698a
Tuan Pham <pvtuanhcmus@gmail.com>
2017-08-11 00:11:03 +0700

    test connect database

---
+++ src/Model/Table/AdminTable.php
@@ -0,0 +1,15 @@
+<?php 
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class AdminTable extends Table
+{
+
+    public function initialize(array $config)
+    {
+        $this->table('product');
+    }
+
+}
+?>
